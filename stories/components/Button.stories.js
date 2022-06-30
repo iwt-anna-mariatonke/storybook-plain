8bf1d8ecbc9a7614278b06c6b089d09192ccbc63
Anna-Maria Tonke <anna-maria.tonke@immowelt.de>
2022-06-30 13:30:11 +0200

    docs: docs structure

--- stories/components/Button.stories.js
+++ stories/components/Button.stories.js
@@ -1,8 +1,11 @@
 import { createButton } from './Button';
+import primary from './examples/primary.html';
+import secondary from './examples/secondary.html';
+import tertiary from './examples/tertiary.html';
 
 // More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Components/Action/Button',
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {
     label: { control: 'text' },
@@ -26,40 +29,46 @@ const Template = ({ label, ...args }) => {
   return createButton({ label, ...args });
 };
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/html/writing-stories/args
-Primary.args = {
+export const Overview = Template.bind({});
+Overview.args = {
+  label: 'Test Button',
   mode: 'primary',
-  label: 'Button',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-  mode: 'secondary',
+export const Primary = () => primary;
+Primary.parameters = {
+  docs: {
+    source: {
+      code: primary,
+    },
+  },
+  controls: { 
+    disable: true 
+  } 
 };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-  label: 'Button',
-  mode: 'tertiary',
+export const Secondary = () => secondary;
+Secondary.parameters = {
+  docs: {
+    source: {
+      code: secondary,
+    },
+  },
+  controls: { 
+    disable: true 
+  } 
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: '200',
-  label: 'Button',
+export const Tertiary = () => tertiary;
+Tertiary.parameters = {
+  docs: {
+    source: {
+      code: tertiary,
+    },
+  },
+  controls: { 
+    disable: true 
+  } 
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: '50',
-  label: 'Button',
-};
 
-export const Danger = Template.bind({});
-// More on args: https://storybook.js.org/docs/html/writing-stories/args
-Danger.args = {
-  danger: true,
-  label: 'Button',
-};
